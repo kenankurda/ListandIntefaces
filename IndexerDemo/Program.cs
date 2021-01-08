2b1c0639d6d5d418b1ff87c6f7f44d3707392e48
kenan <kenan@LAPTOP-5KTTCAE1>
2021-01-08 11:24:36 +0100

    Add project files.

---
+++ IndexerDemo/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerDemo
+{
+    public class Person
+    {
+        string[] people = { "Toon", "Emre", "Ruben", "Khadija", "Timothy", "Jarno", "Donnel" };
+      
+        public string this[int index]
+        {
+            get 
+            { 
+                return people[index]; 
+            }
+            set 
+            { 
+                people[index] = value; 
+            }
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            Console.WriteLine(p[5] + "  " + p[3] + " " + p[2]);
+        }
+    }
+}
---
+++ ListandIntefaces/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListandIntefaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region List
+            List<string> lstString = new List<string>();
+            lstString.Add("Toon");
+            lstString.Add("Emre");
+            lstString.Add("Jarno");
+            lstString.Add("Mauro");
+            lstString.Add("Francis");
+            lstString.Sort();
+            /* Display(lstString)*/
+            
+            #endregion
+
+            #region IList
+            IList<string> ilstString = new List<string>();
+            ilstString.Add("Toon");
+            ilstString.Add("Tim");
+            ilstString.Add("Jan");
+            ilstString.Add("Maria");
+            //ilstString.Sort();
+
+           // DiplayIList(ilstString);
+            //Display(ilstString);
+
+            #endregion
+
+            #region ICollection
+
+            ICollection<string> collString = new List<string>();
+            collString.Add("Toon");
+            collString.Add("Tim");
+            collString.Add("Jan");
+            collString.Add("Maria");
+
+            foreach (var item in collString)
+            {
+                Console.WriteLine(item);
+            }
+
+           // DisplayIcollection(collString);
+
+            #endregion
+
+
+        }
+
+        private static void DisplayIcollection(ICollection<string> lst)
+        {
+            
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        private static void DiplayIList(IList<string> lst) // it can accept any class that implements Ilist
+        {
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        private static void Display(List<string> lst)
+        {
+            
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
